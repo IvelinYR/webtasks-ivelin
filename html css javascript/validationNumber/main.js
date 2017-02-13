665d6d8567993d93e2aea3e95be0664e8ce9d09f
IvelinYR <ivelin.ralev@gmail.com>
2017-02-13 14:36:29 +0200

    Add task from theme html css javascript

---
+++ html css javascript/validationNumber/main.js
@@ -0,0 +1,20 @@
+/**
+ * Created by clouway on 10.02.17.
+ */
+
+function validateFraction() {
+    const number = document.getElementById('number').value
+     if(isFractionValid(number)) {
+         document.getElementById('answer').innerHTML = 'Number is CORECT'
+         document.getElementById('answer').style.color = 'green'
+     }else{
+         document.getElementById('answer').innerHTML = 'Number is INCORECT'
+         document.getElementById('answer').style.color = 'red'
+     }
+}
+
+function isFractionValid(value) {
+    const re = new RegExp(/^[0-9]{1,10}([,.][0-9]{1,5})?$/)
+    return re.test(value);
+}
+
