b41770d0b6fc359fd37037bf8ca1f3c5ba9affd5
IvelinYR <ivelin.ralev@gmail.com>
2017-02-23 11:45:34 +0200

    Add task  Building & Packaging Web Apps
    App is build with webpack.
    I use webpack-dev-server to emits information about the compilation state to the client.
    I use React for building user interfaces.

---
+++ Build with webpack AppBikes/src/container.js
@@ -0,0 +1,40 @@
+import React from '../node_modules/react/react'
+import Footer  from './footer'
+import Header from './header'
+import './container.css';
+import './clearfix.css'
+
+import product1 from './images/img1.png'
+import product2 from './images/img2.png'
+import product3 from './images/img3.png'
+
+const Container = React.createClass({
+    render (){
+    return (
+        <div className='container'>
+          <Header />
+          <div className="main u-clearfix">
+            <section id ="leftPosition" >
+              <h1>Bike models</h1>
+              <div>
+                <img src={product1} alt="" />
+                <img src={product2} alt="" />
+                <img src={product3} alt="" />
+
+              </div>
+            </section>
+            <aside id ="rightPosition" >
+              <h1>Aside</h1>
+              <p><a href="#">Bike 1</a></p>
+              <p><a href="#">Bike 2</a></p>
+              <p><a href="#">Bike 3</a></p>
+              <p><a href="#">Bike 4</a></p>
+            </aside>
+          </div>
+          <Footer />
+        </div>
+    );
+  }
+})
+
+export default Container
---
+++ Build with webpack AppBikes/src/header.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+import './header.css'
+import './clearfix.css'
+
+const Header = React.createClass({
+    render () {
+        return (
+            <div className='header u-clearfix'>
+                <ul>
+                    <li><a href="#home">Home</a></li>
+                    <li><a href="#news">News</a></li>
+                    <li><a href="#contact">Contact</a></li>
+                    <li><a href="#about">About</a></li>
+                </ul>
+            </div>
+        )
+    }
+})
+
+export default Header
